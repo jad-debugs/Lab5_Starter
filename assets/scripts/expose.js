d1756dfdf62d376e01fedff1745c08325c3a3b6b
Jad Isaac <jadjrisaac@gmail.com>
2026-05-05 16:47:41 -0700

    audio correctly selected for each horn

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,16 +5,21 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
     const hornSelect = document.getElementById("horn-select");
     const hornImg = document.querySelector("#expose img");
+    const audio = document.querySelector("audio")
 
     hornSelect.addEventListener("change", () => {
         if (hornSelect.value === "air-horn") {
             hornImg.src = "assets/images/air-horn.svg";
+            audio.src = "assests/audio/air-horn.mp3";
         } else if (hornSelect.value === "car-horn") {
             hornImg.src = "assets/images/car-horn.svg";
+            audio.src = "assests/audio/car-horn.mp3";
+
         } else if (hornSelect.value === "party-horn") {
             hornImg.src = "assets/images/party-horn.svg";
+            audio.src = "assests/audio/party-horn.mp3";
         }
     });
 
-    
+
 }
