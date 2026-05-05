ab69b3fc21f61aacba8ec177183360984031c552
Jad Isaac <jadjrisaac@gmail.com>
2026-05-05 16:39:03 -0700

    image now showing when select a horn

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,18 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    const hornSelect = document.getElementById("horn-select");
+    const hornImg = document.querySelector("#expose img");
+
+    hornSelect.addEventListener("change", () => {
+        if (hornSelect.value === "air-horn") {
+            hornImg.src = "assets/images/air-horn.svg";
+        } else if (hornSelect.value === "car-horn") {
+            hornImg.src = "assets/images/car-horn.svg";
+        } else if (hornSelect.value === "party-horn") {
+            hornImg.src = "assets/images/party-horn.svg";
+        }
+    });
+
+    
 }
