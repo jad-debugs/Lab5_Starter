d006fb2343c748963892b1fa23370c12f53ae16b
Jad Isaac <jadjrisaac@gmail.com>
2026-05-05 22:24:32 -0700

    fixed assets spelled wrong

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -17,14 +17,14 @@ function init() {
     hornSelect.addEventListener("change", () => {
         if (hornSelect.value === "air-horn") {
             hornImg.src = "assets/images/air-horn.svg";
-            audio.src = "assests/audio/air-horn.mp3";
+            audio.src = "assets/audio/air-horn.mp3";
         } else if (hornSelect.value === "car-horn") {
             hornImg.src = "assets/images/car-horn.svg";
-            audio.src = "assests/audio/car-horn.mp3";
+            audio.src = "assets/audio/car-horn.mp3";
 
         } else if (hornSelect.value === "party-horn") {
             hornImg.src = "assets/images/party-horn.svg";
-            audio.src = "assests/audio/party-horn.mp3";
+            audio.src = "assets/audio/party-horn.mp3";
         }
     });
 
